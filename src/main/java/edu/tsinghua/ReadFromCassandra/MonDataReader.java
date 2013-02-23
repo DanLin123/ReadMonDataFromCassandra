5ee3d8f887595b5c2346a8e11e6b1e74de7b3b1b
lindanXmu <lindan_xmu@126.com>
2013-02-23 10:22:27 +0800

    change the column type to long

--- src/main/java/edu/tsinghua/ReadFromCassandra/MonDataReader.java
+++ src/main/java/edu/tsinghua/ReadFromCassandra/MonDataReader.java
@@ -1,32 +1,81 @@
 /**
+ * 
  * 从Cassandra数据库中读取监控数据
- * 如果无法连接，返回提示
- * 如果没有keyspace,返回提示
  * 
  * */
 
 package edu.tsinghua.ReadFromCassandra;
+import java.security.Timestamp;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
+import com.eaio.uuid.UUID;
+import com.eaio.uuid.UUIDGen;
+
+import me.prettyprint.cassandra.serializers.LongSerializer;
 import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.serializers.TimeUUIDSerializer;
 import me.prettyprint.cassandra.service.ColumnSliceIterator;
 import me.prettyprint.cassandra.service.ThriftKsDef;
 import me.prettyprint.hector.api.*;
+import me.prettyprint.hector.api.beans.OrderedRows;
+import me.prettyprint.hector.api.beans.Row;
 import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
 import me.prettyprint.hector.api.ddl.ComparatorType;
 import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
 import me.prettyprint.hector.api.factory.HFactory;
+import me.prettyprint.hector.api.query.QueryResult;
+import me.prettyprint.hector.api.query.RangeSlicesQuery;
 import me.prettyprint.hector.api.query.SliceQuery;
+
 
 
 public class MonDataReader {
 	String url;   //ip:thrift端口
-	String clusterName;    
+	String clusterName;
+	String keyspaceName;
+	String columnFamilyName;
+	Cluster cluster;
+	Keyspace ksp;
+	
 
-	public MonDataReader( String clusterName, String url)
+	/**
+	 * 指定要查询的url, clusterName, keyspace, columnFamily
+	 * */
+	public MonDataReader( String clusterName, String url, String keyspaceName, String columnFamilyName)
 	{
 		this.clusterName = clusterName;
 		this.url = url;
+		this.keyspaceName = keyspaceName;
+		this.columnFamilyName = columnFamilyName;
+	}
+	
+	public void init()
+	{
+
+		//init cluster
+		cluster = HFactory.getOrCreateCluster(this.clusterName, this.url);	
+		System.out.println("Cluster instantiated");
+		
+		KeyspaceDefinition keyspaceDef = cluster.describeKeyspace(this.keyspaceName);
+		// If keyspace does not exist, the CFs don't exist either. => create them.
+		if (keyspaceDef == null) {
+					ColumnFamilyDefinition cfDef = HFactory.createColumnFamilyDefinition(this.keyspaceName,                              
+							columnFamilyName, 
+		                    ComparatorType.BYTESTYPE);
+
+					KeyspaceDefinition newKeyspace = HFactory.createKeyspaceDefinition(keyspaceName,                 
+		                  ThriftKsDef.DEF_STRATEGY_CLASS,  
+		                  1, 
+		                  Arrays.asList(cfDef));
+					//Add the schema to the cluster.
+					//"true" as the second param means that Hector will block until all nodes see the change.
+					cluster.addKeyspace(newKeyspace, true);
+				}
+		
+		ksp = HFactory.createKeyspace(keyspaceName, cluster);
+		System.out.println("Keyspace " +  keyspaceName + " instantiated");
 		
 	}
 	
@@ -37,41 +86,79 @@ public class MonDataReader {
 	 * @param startTime, 查询的开始时间点
 	 * @param endTime,查询的结束时间点
 	 * */
-	public ColumnSliceIterator<String, String, String> getDatas(String keyspace, String columnFamily, String metric, String startTime, String endTime)
+	public ColumnSliceIterator<String, Long, Long> getDatas(String metric, long startTime, long endTime)
 	{
-		//init cluster
-		Cluster cluster = HFactory.getOrCreateCluster(this.clusterName, this.url);	
-		System.out.println("Cluster instantiated");
-		
-		KeyspaceDefinition keyspaceDef = cluster.describeKeyspace(keyspace);
-		
-		// If keyspace does not exist, the CFs don't exist either. => create them.
-		if (keyspaceDef == null) {
-			ColumnFamilyDefinition cfDef = HFactory.createColumnFamilyDefinition("MyKeyspace",                              
-                    "ColumnFamilyName", 
-                    ComparatorType.BYTESTYPE);
-
-			KeyspaceDefinition newKeyspace = HFactory.createKeyspaceDefinition("MyKeyspace",                 
-                  ThriftKsDef.DEF_STRATEGY_CLASS,  
-                  1, 
-                  Arrays.asList(cfDef));
-			//Add the schema to the cluster.
-			//"true" as the second param means that Hector will block until all nodes see the change.
-			cluster.addKeyspace(newKeyspace, true);
-		}
-		
-		Keyspace ksp = HFactory.createKeyspace(keyspace, cluster);
-		System.out.println("Keyspace " +  keyspace + " instantiated");
-		
+		/*UUID startTime = new UUID(UUIDGen.createTime(startTimeInput),UUIDGen.getClockSeqAndNode());
+		UUID endTime = new UUID(UUIDGen.createTime(endTimeInput),UUIDGen.getClockSeqAndNode());*/
 		// Iterates over all columns for the row identified by key "a key"
-		SliceQuery<String, String, String> query = HFactory.createSliceQuery(ksp, StringSerializer.get(),
-		    StringSerializer.get(), StringSerializer.get()).
-		    setKey(metric).setColumnFamily(columnFamily);
+		SliceQuery<String, Long, Long> query = HFactory.createSliceQuery(ksp, StringSerializer.get(),
+	LongSerializer.get(), LongSerializer.get()).
+		    setKey(metric).setColumnFamily(columnFamilyName);
 		
-		ColumnSliceIterator<String, String, String> iterator = new ColumnSliceIterator<String, String, String>(query, startTime, endTime, false);		
+		ColumnSliceIterator<String, Long, Long> iterator = new ColumnSliceIterator<String, Long, Long>(query, startTime, endTime, false);		
 		return iterator;
 	}
 	
+	/**
+	 * get all the data from a row
+	 * */
+	/*public ColumnSliceIterator<String, UUID, String> getRowDatas(String metric)
+	{
+			
+		SliceQuery<String, UUID, String> query = HFactory.createSliceQuery(ksp, StringSerializer.get(),
+				TimeUUIDSerializer.get(), StringSerializer.get()).
+				setKey(metric).setColumnFamily(columnFamilyName);
+		
+		ColumnSliceIterator<String, UUID, String> iterator = new ColumnSliceIterator<String, UUID, String>(query, null, null, false);		
+		return iterator;
+	}*/
+	
+	/**
+	 * This will page through the column family in pages of 100 rows. 
+	 * It will only fetch 10 columns for each row (you will want to page very long rows too).
+	 * This is for a column family with uuids for row keys, 
+	 * strings for column names and longs for values. 
+	 * */
+	public ArrayList<String> getKeys()
+	{
+		int row_count = 100;
+		ArrayList<String> keys = new ArrayList<String>();
+		
+        RangeSlicesQuery<String, String, String> rangeSlicesQuery = HFactory
+            .createRangeSlicesQuery(ksp, StringSerializer.get(), StringSerializer.get(), StringSerializer.get())
+            .setColumnFamily(columnFamilyName)
+            .setRange(null, null, false, 10)
+            .setRowCount(row_count);
 
+        String last_key = null;
+
+        while (true) {
+            rangeSlicesQuery.setKeys(last_key, null);
+            System.out.println(" > " + last_key);
+
+            QueryResult<OrderedRows<String, String, String>> result = rangeSlicesQuery.execute();
+            OrderedRows<String, String, String> rows = result.get();
+            Iterator<Row<String, String, String>> rowsIterator = rows.iterator();
+
+            // we'll skip this first one, since it is the same as the last one from previous time we executed
+            if (last_key != null && rowsIterator != null) rowsIterator.next();   
+
+            while (rowsIterator.hasNext()) {
+              Row<String, String, String> row = rowsIterator.next();
+              last_key = row.getKey();
+
+              if (row.getColumnSlice().getColumns().isEmpty()) {
+                continue;
+              }
+
+
+             keys.add(row.getKey());
+            }
+
+            if (rows.getCount() < row_count)
+                break;
+        }
+        return keys;
+	}
 
 }
--- src/test/java/edu/tsinghua/ReadFromCassandra/MonDataReaderTest.java
+++ src/test/java/edu/tsinghua/ReadFromCassandra/MonDataReaderTest.java
@@ -1,4 +1,5 @@
 package edu.tsinghua.ReadFromCassandra;
+import com.eaio.uuid.UUID;
 
 import me.prettyprint.cassandra.service.ColumnSliceIterator;
 import me.prettyprint.hector.api.beans.HColumn;
@@ -30,23 +31,86 @@ public class MonDataReaderTest extends TestCase
     {
         return new TestSuite( MonDataReaderTest.class );
     }
-
-   
     
     /**
      * test getDatas funciton
      * */
-    public void testGetDatas()
+   public void testGetDatas()
     {
-    	MonDataReader test = new MonDataReader("Test Cluster", "localhost:9160");
+    	MonDataReader test = new MonDataReader("Test Cluster", "127.0.0.1:9160","Monitor","RawData");
+    	test.init();
+    	
+    	ColumnSliceIterator<String, Long, Long>  iterator = 
+    			test.getDatas("ld-VirtualBox#interface#eth0#if_errors##2013-02-22",
+    					System.currentTimeMillis()/1000-100, System.currentTimeMillis());
+    	
+    	
+    	while(iterator.hasNext())
+		{
+			HColumn column = iterator.next();
+			System.out.println( column.getName() + "  "+column.getValue());
+		}
+    }
+    
+    /**
+     * test getRowDatas funciton:get all columns for one row
+     * */ 
+   /* public void testGetRowDatas()
+    {
+    	MonDataReader test = new MonDataReader("Test Cluster", "127.0.0.1:9160","Monitor","RawData");
+    	test.init();
+    	
     	ColumnSliceIterator<String, String, String>  iterator = 
-    			test.getDatas("RawData", "Time", "ld-VirtualBox#processes##ps_state#sleeping",  "135928455", "135928485");
+    			test.getRowDatas("ld-VirtualBox#interface#eth0#if_octets##2013-02-22");
     	
     	while(iterator.hasNext())
 		{
 			HColumn column = iterator.next();
 			System.out.println(column.getName()+ "  "+column.getValue());
 		}
-    }
+    }*/
+    
+//    org.apache.cassandra.db:type=ColumnFamilies,keyspace=RawData,columnfamily=Time
+   /* public void testGetKeys()
+    {
+    	MonDataReader test = new MonDataReader("Test Cluster", "127.0.0.1:9160","Monitor","RawData");
+    	test.init();
+    	ArrayList<String> keys = test.getKeys();
+    	
+    	//wirte to file
+    	// Create file 
+    	
+		try {
+			FileWriter fstream = new FileWriter("/home/ld/collectdPythonCode/keys");
+			BufferedWriter out = new BufferedWriter(fstream);
+	    	out.write("-----------keys-----------------");
+	    	for(String key : keys)
+	    	{
+	    		out.append(key+"\n");
+	    		System.out.println(key);
+	    	}
+	    	out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    }*/
+   
+	public static java.util.UUID toUUID(byte[] uuid) {
+		long msb = 0;
+		long lsb = 0;
+		assert uuid.length == 16;
+		for (int i = 0; i < 8; i++)
+			msb = (msb << 8) | (uuid[i] & 0xff);
+		for (int i = 8; i < 16; i++)
+			lsb = (lsb << 8) | (uuid[i] & 0xff);
+		long mostSigBits = msb;
+		long leastSigBits = lsb;
+
+		com.eaio.uuid.UUID u = new com.eaio.uuid.UUID(msb, lsb);
+		return java.util.UUID.fromString(u.toString());
+	}
  
 }
